a03a9d691d6e74d30e5d97bdd24768169c8f813b
RealHackRH593 <60048267+RealHackRH593@users.noreply.github.com>
2020-01-30 15:28:59 -0500

    Add files via upload

---
+++ mundoccrh.sh
@@ -0,0 +1,186 @@
+#!/system/xbin/bash
+
+clear
+cd module
+cat baner.txt | lolcat
+python3 acceso.py
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+read -p "%%%%%%%% MUNDO BINS ★RH★_593 %%%%%%%% : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "MundoNetRH" | lolcat
+php cc-us.php
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+figlet "MundoNetRH" | lolcat
+php cc-au.php
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+figlet "MundoNetRH" | lolcat
+php cc-as.php
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+figlet "MundoNetRH" | lolcat
+php cc-bg.php
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+figlet "MundoNetRH" | lolcat
+php cc-br.php
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+figlet "MundoNetRH" | lolcat
+php cc-ca.php
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+figlet "MundoNetRH" | lolcat
+php cc-dk.php
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+figlet "MundoNetRH" | lolcat
+php cc-ee.php
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+figlet "MundoNetRH" | lolcat
+php cc-fi.php
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+figlet "MundoNetRH" | lolcat
+php cc-fr.php
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+figlet "MundoNetRH" | lolcat
+php cc-gr.php
+fi
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then
+clear
+figlet "MundoNetRH" | lolcat
+php cc-gl.php
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+figlet "MundoNetRH" | lolcat
+php cc-hu.php
+fi
+
+if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+figlet "MundoNetRH" | lolcat
+php cc-is.php
+fi
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+figlet "MundoNetRH" | lolcat
+php cc-nl.php
+fi
+
+if [ $bro = 16 ] || [ $bro = 16 ]
+then
+clear
+figlet "MundoNetRH" | lolcat
+php cc-no.php
+fi
+
+if [ $bro = 17 ] || [ $bro = 17 ]
+then
+clear
+figlet "MundoNetRH" | lolcat
+php cc-pl.php
+fi
+
+if [ $bro = 18 ] || [ $bro = 18 ]
+then
+clear
+figlet "MundoNetRH" | lolcat
+php cc-pt.php
+fi
+
+if [ $bro = 19 ] || [ $bro = 19 ]
+then
+clear
+figlet "MundoNetRH" | lolcat
+php cc-sl.php
+fi
+
+if [ $bro = 20 ] || [ $bro = 20 ]
+then
+clear
+figlet "MundoNetRH" | lolcat
+php cc-sp.php
+fi
+
+if [ $bro = 21 ] || [ $bro = 21 ]
+then
+clear
+figlet "MundoNetRH" | lolcat
+php cc-sw.php
+fi
+
+if [ $bro = 22 ] || [ $bro = 22 ]
+then
+clear
+figlet "MundoNetRH" | lolcat
+php cc-sz.php
+fi
+
+if [ $bro = 23 ] || [ $bro = 23 ]
+then
+clear
+figlet "MundoNetRH" | lolcat
+php cc-tn.php
+fi
+
+if [ $bro = 24 ] || [ $bro = 24 ]
+then
+figlet "MundoNetRH" | lolcat
+sleep 2
+python3 salida.py
+sleep 1
+exit
+fi
+
